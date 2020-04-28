f63faab959edea2788d5a3d5a7eaa36542d74ffa
byungchan <bl458@cornell.edu>
2020-04-28 21:21:23 +0900

    Nav bar rough draft

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
 import Quiz from '../quiz/Quiz';
+import Nav from '../nav/Nav';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 
-const App = () =>
-  {return (<Quiz />);}
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path='/' exact component={Home}/>
+          <Route path='/quiz' component={Quiz}/>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+const Home=()=>(
+  <div>
+      <h1>Home Page</h1>
+  </div>
+)
 
 export default App;
