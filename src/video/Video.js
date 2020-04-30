6d21606c3fdd8527ab36ea01c1e12a71b6768e89
byungchan <bl458@cornell.edu>
2020-05-01 01:33:10 +0900

    made video placeholder

--- src/video/Video.js
+++ src/video/Video.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
+import {Player} from 'video-react';
 import './Video.css'
+import '../../node_modules/video-react/dist/video-react.css';
 
 class Video extends Component {
   render() {
     return(
       <div className="container">
-        <div className="title">Video Learning</div>
+        <div className="video-title">Video Learning</div>
         <div className="video-board">
-          Video Learning goes here
+          <div className="video-inner">
+            <Player
+               src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+            />
+          </div>
+          <div className="subtitle">
+            Subtitle goes here
+          </div>
         </div>
       </div>
     );
