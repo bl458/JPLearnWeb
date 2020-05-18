cb774bd2d5f976fe547fc07d3e2780bb7d18e7ab
byungchan <bl458@cornell.edu>
2020-05-18 20:13:59 +0900

    get highlighted text working

--- src/video/Subtitle.js
+++ src/video/Subtitle.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
+const helper = () => {
+  if (window.getSelection().toString() !== '') {
+    console.log(window.getSelection().toString())
+  }
+}
+
 const Subtitle = ({engSub, jpSub}) => (
   <div className="subtitle">
     {jpSub}<br/>{engSub}
+    {helper()}
   </div>
 )
 
