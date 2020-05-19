613203bb60fce2fafd0d60bd115e6d32e12339cd
byungchan <bl458@cornell.edu>
2020-05-19 17:58:33 +0900

    Hiragana conversion for highlighted text working on console

--- src/video/Subtitle.js
+++ src/video/Subtitle.js
@@ -3,24 +3,28 @@ import Kuroshiro from 'kuroshiro';
 import KuromojiAnalyzer from "kuroshiro-analyzer-kuromoji";
 
 const helper = () => {
-  // if (window.getSelection().toString() !== '') {
-  //   console.log(window.getSelection().toString())
-  // }
-
+  console.log("Entered helper")
   var kuroshiro = new Kuroshiro();
-  kuroshiro.init(new KuromojiAnalyzer({ dictPath: "/dict" }))
-    .then(function () {
-        return kuroshiro.convert("感じ取れたら手を繋ごう、重なるのは人生のライン and レミリア最高！", { to: "hiragana" });
-    })
-    .then(function(result){
+
+  if (window.getSelection().toString() !== '') {
+    kuroshiro.init(new KuromojiAnalyzer({ dictPath: "/dict" }))
+      .then(() => {
+        return kuroshiro.convert(
+          window.getSelection().toString(),
+           { to: "hiragana" });
+      })
+      .then((result) => {
         console.log(result);
-    })
+      })
+  }
+
+
+
 }
 
 const Subtitle = ({engSub, jpSub}) => (
-  <div className="subtitle">
+  <div className="subtitle" onClick={helper}>
     {jpSub}<br/>{engSub}
-    {helper()}
   </div>
 )
 
