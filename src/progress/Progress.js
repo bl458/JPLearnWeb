f47df2261a4f89c979b89ebcc9ae639f4d48db3c
byungchan <bl458@cornell.edu>
2020-06-18 14:20:43 +0900

    Updated placeholder for deck progress page

--- src/progress/Progress.js
+++ src/progress/Progress.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react'
 
 class Progress extends Component {
+  onCheck = () => {
+
+  }
+
   render() {
     return (
-      <div>
-        <h1>This is the Progress page! ID is {this.props.id}</h1>
+      <div className="quiz-board">
+        <div className="quiz-inner">
+          <h1>This is the Progress page! ID is {this.props.id}</h1>
+          <label>Deck:     </label>
+          <select className="deck-select">
+            <option value="N1">N1</option>
+            <option value="N2">N2</option>
+          </select>
+          <button className="playBtn" onClick={this.onCheck}>Check!</button>
+        </div>
       </div>
     )
   }
