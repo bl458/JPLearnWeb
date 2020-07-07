0afaeb14c6ad95251529dc95161b2a1ff980edf8
Byungchan <bl458@cornell.edu>
2020-07-07 23:33:51 +0900

    Fix: mysql conn bug

--- client/src/quiz/Quiz.js
+++ client/src/quiz/Quiz.js
@@ -1,160 +1,212 @@
-import React, {Component} from 'react'
-import './Quiz.css'
-import QuizStart from './QuizStart'
-import QuizState from './QuizState'
-import QuizResult from './QuizResult'
+import React, { Component } from "react";
+import "./Quiz.css";
+import QuizStart from "./QuizStart";
+import QuizState from "./QuizState";
+import QuizResult from "./QuizResult";
 
 class Quiz extends Component {
-  state={
+  state = {
     playing: 0, //Start page: 0, In-game: 1, Results page: 2
-    deck: 'N1',
+    deck: "N1",
     numberq: 1, //Total number of questions in the quiz (min 1, max 10)
     score: 0,
-    qBank: [{kanji:"",hiragana:"",english:""}], // question bank for each game
-    rQBank: [{kanji:"",hiragana:"",english:""}], // review question bank
-    cRQBank: [{kanji:"",hiragana:"",english:""}],
-    answer: '',
+    qBank: [{ kanji: "", hiragana: "", english: "" }], // question bank for each game
+    rQBank: [{ kanji: "", hiragana: "", english: "" }], // review question bank
+    cRQBank: [{ kanji: "", hiragana: "", english: "" }],
+    answer: "",
     questionPage: 0, //Question Page: 0, Answer page: 1
-    firstReview: 1 //Review mode for the first time or naw
-  }
+    firstReview: 1, //Review mode for the first time or naw
+  };
 
   changePlaying = (quizPlaying) => {
     this.setState({
-      playing: quizPlaying
-    })
-  }
+      playing: quizPlaying,
+    });
+  };
 
   selectRandomArr = (arr, count) => {
-    return Promise.resolve(arr.sort(() => 0.5 - Math.random()).slice(0, count))
-  }
+    console.log(arr);
+    return Promise.resolve(arr.sort(() => 0.5 - Math.random()).slice(0, count));
+  };
 
   getQuestions = () => {
-    if (this.state.deck==='N1' || this.state.deck==='N2' || this.state.deck==='review' || this.state.deck==='video_vocab') {
+    if (
+      this.state.deck === "N1" ||
+      this.state.deck === "N2" ||
+      this.state.deck === "review" ||
+      this.state.deck === "video_vocab"
+    ) {
       fetch(`http://localhost:4000/load_deck?deck=${this.state.deck}`)
-      .then(response => response.json())
-      .then(response => this.selectRandomArr(response.data, 10))
-      .then(questions => {
-        this.setState({qBank: questions.slice(0,this.state.numberq)})
-      })
+        .then((response) => response.json())
+        .then((response) => this.selectRandomArr(response.data, 10))
+        .then((questions) => {
+          this.setState({ qBank: questions.slice(0, this.state.numberq) });
+        });
     }
     //
     else {
-      console.log('Not a valid deck')
+      console.log("Not a valid deck");
     }
-  }
+  };
 
   onNumberqChange = (newNumberq) =>
-    this.setState({numberq: newNumberq.target.value>0&&newNumberq.target.value<10
-      ? newNumberq.target.value : 10})
+    this.setState({
+      numberq:
+        newNumberq.target.value > 0 && newNumberq.target.value < 10
+          ? newNumberq.target.value
+          : 10,
+    });
 
-  onDeckChange = (newDeck) => this.setState({deck: newDeck.target.value})
+  onDeckChange = (newDeck) => this.setState({ deck: newDeck.target.value });
 
-  onAnswerChange = (newAnswer) => this.setState({answer: newAnswer.target.value})
+  onAnswerChange = (newAnswer) =>
+    this.setState({ answer: newAnswer.target.value });
 
   onStart = () => {
-    this.getQuestions()
+    this.getQuestions();
     this.setState({
       score: 0,
-      playing: this.state.playing===0 ? 1 : 0,
-      questionPage: 0
-    })
-    console.log("Finished onStart. numberq: " +this.state.numberq +" qBank: " +this.state.qBank.length)
+      playing: this.state.playing === 0 ? 1 : 0,
+      questionPage: 0,
+    });
+    console.log(
+      "Finished onStart. numberq: " +
+        this.state.numberq +
+        " qBank: " +
+        this.state.qBank.length
+    );
     // console.log('Quiz id: ', this.props.id)
-  }
+  };
 
   onAgain = () => {
     this.setState({
-      qBank: [{kanji:"",hiragana:"",english:""}],
-      rQBank: [{kanji:"",hiragana:"",english:""}],
-      cRQBank: [{kanji:"",hiragana:"",english:""}],
+      qBank: [{ kanji: "", hiragana: "", english: "" }],
+      rQBank: [{ kanji: "", hiragana: "", english: "" }],
+      cRQBank: [{ kanji: "", hiragana: "", english: "" }],
       score: 0,
-      playing: this.state.playing===0 ? 1 : 0,
+      playing: this.state.playing === 0 ? 1 : 0,
       firstReview: 1,
-    })
-    console.log("Finished onAgain. numberq: " +this.state.numberq +" qBank: " +this.state.qBank.length)
-  }
+    });
+    console.log(
+      "Finished onAgain. numberq: " +
+        this.state.numberq +
+        " qBank: " +
+        this.state.qBank.length
+    );
+  };
 
   onReview = () => {
-    var rQBank_cp = this.state.rQBank
-    rQBank_cp.splice(0,1)
+    var rQBank_cp = this.state.rQBank;
+    rQBank_cp.splice(0, 1);
     this.setState({
-      numberq: this.state.firstReview===1 ? this.state.rQBank.length : this.state.cRQBank.length,
-      cRQBank: this.state.firstReview===1 ? JSON.parse(JSON.stringify(this.state.rQBank)) : JSON.parse(JSON.stringify(this.state.cRQBank)),
-      qBank: this.state.firstReview===1 ? JSON.parse(JSON.stringify(this.state.rQBank)) : JSON.parse(JSON.stringify(this.state.cRQBank)),
+      numberq:
+        this.state.firstReview === 1
+          ? this.state.rQBank.length
+          : this.state.cRQBank.length,
+      cRQBank:
+        this.state.firstReview === 1
+          ? JSON.parse(JSON.stringify(this.state.rQBank))
+          : JSON.parse(JSON.stringify(this.state.cRQBank)),
+      qBank:
+        this.state.firstReview === 1
+          ? JSON.parse(JSON.stringify(this.state.rQBank))
+          : JSON.parse(JSON.stringify(this.state.cRQBank)),
       score: 0,
       playing: 1,
       firstReview: 0,
-      rQBank: [{kanji:"",hiragana:"",english:""}]
-    })
-    console.log(this.state.cRQBank)
-  }
+      rQBank: [{ kanji: "", hiragana: "", english: "" }],
+    });
+    console.log(this.state.cRQBank);
+  };
 
   onSubmit = () => {
-    console.log("Started onSubmit. qBank: " +this.state.qBank.length)
+    console.log("Started onSubmit. qBank: " + this.state.qBank.length);
     // Updates score, shows answer
     this.setState({
-      score: this.state.qBank[0].hiragana===this.state.answer.trim() ?
-        this.state.score+1 : this.state.score,
+      score:
+        this.state.qBank[0].hiragana === this.state.answer.trim()
+          ? this.state.score + 1
+          : this.state.score,
       questionPage: 1,
-      rQBank: this.state.qBank[0].hiragana===this.state.answer.trim() ?
-        this.state.rQBank : [...this.state.rQBank, this.state.qBank[0]]
-    })
+      rQBank:
+        this.state.qBank[0].hiragana === this.state.answer.trim()
+          ? this.state.rQBank
+          : [...this.state.rQBank, this.state.qBank[0]],
+    });
 
     // Add wrong answer to review deck in db when you're not playing quiz with the review deck
-    if (this.state.qBank[0].hiragana!==this.state.answer.trim() && this.state.deck!=='review') {
-      fetch(`http://localhost:4000/review?googleId=${this.props.id}&deck=${this.state.deck}&kanji=${this.state.qBank[0].kanji}&hiragana=${this.state.qBank[0].hiragana}&english=${this.state.qBank[0].english}`)
+    if (
+      this.state.qBank[0].hiragana !== this.state.answer.trim() &&
+      this.state.deck !== "review"
+    ) {
+      fetch(
+        `http://localhost:4000/review?googleId=${this.props.id}&deck=${this.state.deck}&kanji=${this.state.qBank[0].kanji}&hiragana=${this.state.qBank[0].hiragana}&english=${this.state.qBank[0].english}`
+      );
     }
 
     // Add to deck progress in db
-    fetch(`http://localhost:4000/progress?googleId=${this.props.id}&deck=${this.state.deck}&kanji=${this.state.qBank[0].kanji}`)
+    fetch(
+      `http://localhost:4000/progress?googleId=${this.props.id}&deck=${this.state.deck}&kanji=${this.state.qBank[0].kanji}`
+    );
 
-    console.log("Finished onSubmit. numberq: " +this.state.numberq +" qBank: " +this.state.qBank.length +" rQBank: " +this.state.rQBank.length)
-  }
+    console.log(
+      "Finished onSubmit. numberq: " +
+        this.state.numberq +
+        " qBank: " +
+        this.state.qBank.length +
+        " rQBank: " +
+        this.state.rQBank.length
+    );
+  };
 
   onSkip = () => {
     // Shows answer
     this.setState({
       questionPage: 1,
-      rQBank: [...this.state.rQBank, this.state.qBank[0]]
-    })
-    fetch(`http://localhost:4000/review?googleId=${this.props.id}&deck=${this.state.deck}&kanji=${this.state.qBank[0].kanji}&hiragana=${this.state.qBank[0].hiragana}&english=${this.state.qBank[0].english}`)
-    fetch(`http://localhost:4000/progress?googleId=${this.props.id}&deck=${this.state.deck}&kanji=${this.state.qBank[0].kanji}`)
-  }
+      rQBank: [...this.state.rQBank, this.state.qBank[0]],
+    });
+    fetch(
+      `http://localhost:4000/review?googleId=${this.props.id}&deck=${this.state.deck}&kanji=${this.state.qBank[0].kanji}&hiragana=${this.state.qBank[0].hiragana}&english=${this.state.qBank[0].english}`
+    );
+    fetch(
+      `http://localhost:4000/progress?googleId=${this.props.id}&deck=${this.state.deck}&kanji=${this.state.qBank[0].kanji}`
+    );
+  };
 
-  onEnterPress = (func,e) => {
-    var code=e.keyCode || e.which
-    if (code===13) {
-      func()
+  onEnterPress = (func, e) => {
+    var code = e.keyCode || e.which;
+    if (code === 13) {
+      func();
     }
-  }
+  };
 
   onNext = () => {
     // Updates question, resets answer-input value
-    var qBank_cp=this.state.qBank
-    qBank_cp.splice(0,1)
+    var qBank_cp = this.state.qBank;
+    qBank_cp.splice(0, 1);
     this.setState({
       qBank: qBank_cp,
-      answer: '',
+      answer: "",
       questionPage: 0, // Back to question
-      playing: this.state.qBank.length===0 ? 2 : this.state.playing // If user answered all, quit quiz
-    })
-  }
+      playing: this.state.qBank.length === 0 ? 2 : this.state.playing, // If user answered all, quit quiz
+    });
+  };
 
   /*Actual rendering from here*/
   render() {
     return (
       <div className="container">
         <div className="title">Quiz</div>
-        {this.state.playing===0&&
-        <QuizStart
-          numberq={this.state.numberq}
-          onNumberqChange={this.onNumberqChange}
-          deck={this.state.deck}
-          onDeckChange={this.onDeckChange}
-          onStart={this.onStart}
-        />
-        }
-        {this.state.playing===1&&
+        {this.state.playing === 0 && (
+          <QuizStart
+            numberq={this.state.numberq}
+            onNumberqChange={this.onNumberqChange}
+            deck={this.state.deck}
+            onDeckChange={this.onDeckChange}
+            onStart={this.onStart}
+          />
+        )}
+        {this.state.playing === 1 && (
           <QuizState
             qBank={this.state.qBank}
             numberq={this.state.numberq}
@@ -168,8 +220,8 @@ class Quiz extends Component {
             onNext={this.onNext}
             displayDeckName={this.props.displayDeckName}
           />
-        }
-        {this.state.playing===2&&
+        )}
+        {this.state.playing === 2 && (
           <QuizResult
             score={this.state.score}
             numberq={this.state.numberq}
@@ -177,10 +229,10 @@ class Quiz extends Component {
             review={this.onReview}
             firstReview={this.state.firstReview}
           />
-        }
+        )}
       </div>
-    )
+    );
   }
 }
 
-export default Quiz
+export default Quiz;
--- server/server.js
+++ server/server.js
@@ -1,130 +1,117 @@
-const express = require('express')
-const cors = require('cors')
-const mysql = require('mysql')
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
 
-const app = express()
-const SELECT_ALL_LOGIN = "SELECT * FROM jquiz_google_login"
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Lbc1skygrin',
-  database: 'nodejs',
-  charset : 'utf8mb4'
-})
+const app = express();
+const SELECT_ALL_LOGIN = "SELECT * FROM jquiz_google_login";
+const connection = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "Lbc1skygrin!",
+  database: "nodejs",
+  charset: "utf8mb4",
+});
 
-connection.connect(err => {
-  if(err) {
-    return err
+connection.getConnection((err) => {
+  if (err) {
+    return err;
   }
-})
+});
 
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Go to /login for login data')
-})
+app.get("/", (req, res) => {
+  res.send("Go to /login for login data");
+});
 
-app.get('/login_data', (req, res) => {
+app.get("/login_data", (req, res) => {
   connection.query(SELECT_ALL_LOGIN, (err, results) => {
     if (err) {
-      return res.send(err)
-    }
-    else {
+      return res.send(err);
+    } else {
       return res.json({
-        data: results
-      })
+        data: results,
+      });
     }
-  })
-})
+  });
+});
 
-app.get('/register', (req, res) => {
-  const {googleId, name, email} = req.query
-  const REGISTER_QUERY = `INSERT INTO jquiz_google_login(google_id, name, email, created) VALUES('${googleId}', '${name}' , '${email}', 1)`
+app.get("/register", (req, res) => {
+  const { googleId, name, email } = req.query;
+  const REGISTER_QUERY = `INSERT INTO jquiz_google_login(google_id, name, email, created) VALUES('${googleId}', '${name}' , '${email}', 1)`;
   connection.query(REGISTER_QUERY, (err, results) => {
     if (err) {
-      return res.send(err)
+      return res.send(err);
+    } else {
+      return res.send("Successfully registered user");
     }
-    else {
-      return res.send('Successfully registered user')
-    }
-  })
-})
+  });
+});
 
-app.get('/video_vocab', (req, res) => {
-  const {googleId, vidId, kanji, hiragana, english} = req.query
-  const VIDEO_VOCAB_QUERY =
-    `INSERT INTO jquiz_video_vocab(google_id, video_id, kanji, hiragana, english) VALUES('${googleId}', '${vidId}' , '${kanji}', '${hiragana}', '${english}')`
+app.get("/video_vocab", (req, res) => {
+  const { googleId, vidId, kanji, hiragana, english } = req.query;
+  const VIDEO_VOCAB_QUERY = `INSERT INTO jquiz_video_vocab(google_id, video_id, kanji, hiragana, english) VALUES('${googleId}', '${vidId}' , '${kanji}', '${hiragana}', '${english}')`;
   connection.query(VIDEO_VOCAB_QUERY, (err, results) => {
     if (err) {
-      return res.send(err)
+      return res.send(err);
+    } else {
+      return res.send("Successfully added video vocab");
     }
-    else {
-      return res.send('Successfully added video vocab')
+  });
+});
+
+app.get("/review", (req, res) => {
+  const { googleId, deck, kanji, hiragana, english } = req.query;
+  const REVIEW_QUERY = `INSERT INTO jquiz_review(google_id, deck, kanji, hiragana, english) VALUES('${googleId}', '${deck}' , '${kanji}', '${hiragana}', '${english}')`;
+  connection.query(REVIEW_QUERY, (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.send("Successfully added vocab to review");
     }
-  })
- })
+  });
+});
 
- app.get('/review', (req, res) => {
-   const {googleId, deck, kanji, hiragana, english} = req.query
-   const REVIEW_QUERY =
-     `INSERT INTO jquiz_review(google_id, deck, kanji, hiragana, english) VALUES('${googleId}', '${deck}' , '${kanji}', '${hiragana}', '${english}')`
-   connection.query(REVIEW_QUERY, (err, results) => {
-     if (err) {
-       return res.send(err)
-     }
-     else {
-       return res.send('Successfully added vocab to review')
-     }
-   })
-  })
+app.get("/progress", (req, res) => {
+  const { googleId, deck, kanji } = req.query;
+  const PROGRESS_QUERY = `INSERT INTO jquiz_progress(google_id, deck, kanji) VALUES('${googleId}', '${deck}' , '${kanji}')`;
+  connection.query(PROGRESS_QUERY, (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.send("Successfully added vocab to progress");
+    }
+  });
+});
 
-  app.get('/progress', (req, res) => {
-    const {googleId, deck, kanji} = req.query
-    const PROGRESS_QUERY =
-      `INSERT INTO jquiz_progress(google_id, deck, kanji) VALUES('${googleId}', '${deck}' , '${kanji}')`
-    connection.query(PROGRESS_QUERY, (err, results) => {
-      if (err) {
-        return res.send(err)
-      }
-      else {
-        return res.send('Successfully added vocab to progress')
-      }
-    })
-   })
+app.get("/view_progress", (req, res) => {
+  const { googleId, deck } = req.query;
+  const VIEW_PROGRESS_QUERY = `SELECT * FROM jquiz_progress WHERE google_id=${googleId} and deck=${deck}`;
+  connection.query(VIEW_PROGRESS_QUERY, (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.json({
+        data: results,
+      });
+    }
+  });
+});
 
-   app.get('/view_progress', (req, res) => {
-     const {googleId, deck} = req.query
-     const VIEW_PROGRESS_QUERY =
-       `SELECT * FROM jquiz_progress WHERE google_id=${googleId} and deck=${deck}`
-     connection.query(VIEW_PROGRESS_QUERY, (err, results) => {
-       if (err) {
-         return res.send(err)
-       }
-       else {
-         return res.json({
-           data: results
-         })
-       }
-     })
-    })
-
-    app.get('/load_deck', (req, res) => {
-      const {deck} = req.query
-      const LOAD_DECK_QUERY =
-        `SELECT * FROM jquiz_${deck}`
-      connection.query(LOAD_DECK_QUERY, (err, results) => {
-        if (err) {
-          return res.send(err)
-        }
-        else {
-          return res.json({
-            data: results
-          })
-        }
-      })
-     })
-
+app.get("/load_deck", (req, res) => {
+  const { deck } = req.query;
+  const LOAD_DECK_QUERY = `SELECT * FROM jquiz_${deck}`;
+  connection.query(LOAD_DECK_QUERY, (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.json({
+        data: results,
+      });
+    }
+  });
+});
 
 app.listen(4000, () => {
-  console.log('Server on port 4000 working')
-})
+  console.log("Server on port 4000 working");
+});
